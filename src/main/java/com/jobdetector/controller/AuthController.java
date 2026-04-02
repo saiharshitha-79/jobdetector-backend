b685c10e51ce2f23c77c42b8983922a00f49d96e
Gujjula Sai Harshitha <gujjulasaiharshitha@gmail.com>
2026-04-02 15:45:59 +0530

    Update AuthController.java

--- src/main/java/com/jobdetector/controller/AuthController.java
+++ src/main/java/com/jobdetector/controller/AuthController.java
@@ -12,7 +12,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins = {"http://localhost:3000", "http://localhost:3001"})
+@CrossOrigin(origins = {"https://jobdetector-backend.onrender.com", "http://localhost:3001"})
 public class AuthController {
     
     @Autowired
